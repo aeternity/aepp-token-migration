77c5064997a9a3062da9260241dc505741df4f94
Emil Sommerfeldt <emil@jungfeldt.de>
2018-10-04 17:01:44 +0200

    make custom btn component

---
+++ src/components/AeBtn.vue
@@ -0,0 +1,45 @@
+<template>
+  <router-link class="ae-btn"
+    :to="to"
+    :class="{
+      'ae-btn_primary': is_primary,
+      'ae-btn_secondary': is_secondary,
+      'ae-btn_neutral': is_neutral
+    }"
+  >
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'ae-btn',
+  props: {
+    is_secondary: { type: Boolean, default: false },
+    is_primary: { type: Boolean, default: false },
+    is_neutral: { type: Boolean, default: false },
+    to: { type: [String, Object], default: undefined }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .ae-btn{
+    border-radius: 1.5rem;
+    padding: $spacer-m $spacer-xl;
+    color: $white;
+    font-size: 13px;
+    text-transform: uppercase;
+    font-weight: 700;
+    &_primary{
+      background-color: $btn_primary;
+    }
+    &_secondary{
+      background-color: $btn_secondary;
+    }
+    &_neutral{
+      background-color: $btn_neutral;
+    }
+  }
+</style>
