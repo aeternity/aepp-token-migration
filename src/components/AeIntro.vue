2251a949d67f8f15f24033c9f72ae008ce25823a
Emil Sommerfeldt <emil@jungfeldt.de>
2018-10-04 17:02:15 +0200

    make title and intro text component

---
+++ src/components/AeIntro.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="ae-intro" >
+    <h2 class="ae-intro__title">
+      {{ title }}
+    </h2>
+    <p v-if="intro" class="ae-intro__text">
+      {{ intro }}
+    </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'ae-intro',
+  props: ['title', 'intro']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .ae-intro{
+    text-align: center;
+    max-width: 28em;
+    @include font-size(m);
+    margin: 0 auto $spacer-xl ;
+    &__title {
+       @include font-size(xxl);
+       line-height: 1.5em;
+    }
+    &__text {
+      
+    }
+  }
+</style>
