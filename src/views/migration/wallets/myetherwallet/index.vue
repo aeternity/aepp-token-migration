359bcdb2a2ffa08d41565eaa56375a80e188604b
Philipp Klein <klein.philipp@gmail.com>
2018-10-19 19:12:04 +0200

    generate link for myetherwallet, basic version without styling

--- src/views/migration/wallets/myetherwallet/index.vue
+++ src/views/migration/wallets/myetherwallet/index.vue
@@ -13,15 +13,24 @@
         </template>
       </app-intro>
       <app-panel padding shadow>
-        <div>
-          <textarea v-model="inputData" />
-        </div>
+        <p>Input your desired AE Token Amount to migrate:</p>
+        <p>Plese make sure you don't exceed your balance</p>
+        <input type="text" v-model="amount" style="width: 100%; height: 40px; border: none; margin: 50px 0" />
+
+        <p>generated Payload:</p>
+        <textarea readonly v-model="payLoad" style="width: 100%; height: 110px; margin: 50px 0" />
+
+        <p>decode Payload with this tool</p>
         <app-panel>
-          <div>{{inputData}}</div>
+          <textarea v-model="inputData" style="width: 100%; height: 110px; margin: 50px 0" />
         </app-panel>
         <app-panel>
-          <div>{{decodedInputData}}</div>
+          <textarea v-model="JSON.stringify(decodedInputData)" style="width: 100%; height: 110px; margin: 50px 0" />
         </app-panel>
+
+        <a target='_blank' :href='mewLink'>
+          {{mewLink}}
+        </a>
       </app-panel>
     </app-view>
   </app-view>
@@ -46,7 +55,8 @@ export default {
   name: 'myetherwallet',
   data: function () {
     return {
-      inputData: 'cae9ca510000000000000000000000004ecd812b010d9db16b0fb7143a79786b65b89b0900000000000000000000000000000000000000000000000000038d7ea4c680000000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000007400000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000034616b5f776d5a55765a5772566962504d32507553476867576d4d51586368455767525462774270377459556350794259486e7052000000000000000000000000'
+      amount: "0",
+      inputData: ''
     }
   },
   components: {
@@ -57,24 +67,61 @@ export default {
     AeIcon,
     AeInput
   },
+  mounted() {
+    console.log(this.$web3.utils.padLeft(this.$web3.utils.toHex(this.$web3.utils.toWei("123456")).slice(2), 64))
+  },
   methods: {
     inputChange (e) {
       console.log(e)
+    },
+    migrate (amount, walletAddress) {
+      alert(amount +  walletAddress)
     }
   },
   computed: {
+    payLoad () {
+      return `${
+        'cae9ca51'
+      }${
+        '0000000000000000000000004ecd812b010d9db16b0fb7143a79786b65b89b09'
+      }${
+        this.$web3.utils.padLeft(this.$web3.utils.toHex(this.$web3.utils.toWei(this.amount)).slice(2), 64)
+      }${
+        '0000000000000000000000000000000000000000000000000000000000000060'
+      }${
+        '0000000000000000000000000000000000000000000000000000000000000074'
+      }${
+        '0000000000000000000000000000000000000000000000000000000000000080'
+      }${
+        '0000000000000000000000000000000000000000000000000000000000000034'
+      }${
+        this.$web3.utils.padRight(this.$web3.utils.fromUtf8(this.walletAddress).slice(2), 64)
+      }`
+    },
+    mewLink () {
+      return `https://www.myetherwallet.com/?to=0x35d8830ea35e6df033eedb6d5045334a4e34f9f9&value=0&gaslimit=300000&data=${this.payLoad}#send-transaction`
+        // '616b5f776d5a55765a5772566962504d32507553476867576d4d515863684557'
+        // }${
+        // '67525462774270377459556350794259486e7052000000000000000000000000'
+        // }#send-transaction`
+    },
     decodedInputData () {
-      return {
-        // lol:  abi.simpleDecode("approveAndCall(address,uint256,bytes):(bool)", "0x" + this.inputData),
-        // lol: abi.decode(tokenAbi, 'approveAndCall(address uint256 bytes)', this.inputData),
-        callFunction: this.inputData.substr(0, 8) === 'cae9ca51',
-        burnContract: this.inputData.substr(8, 64) === '0000000000000000000000004ecd812b010d9db16b0fb7143a79786b65b89b09',
-        amount: Web3.utils.fromWei(Web3.utils.toBN('0x' + this.inputData.substr(72, 64)).toString()),
-        aeternity: new Web3().utils.hexToString((new Web3()).eth.abi.decodeParameter('bytes', this.inputData.substr(136)))
+      try {
+        return {
+          // lol:  abi.simpleDecode("approveAndCall(address,uint256,bytes):(bool)", "0x" + this.inputData),
+          // lol: abi.decode(tokenAbi, 'approveAndCall(address uint256 bytes)', this.inputData),
+          callFunction: this.inputData.substr(0, 8) === 'cae9ca51',
+          burnContract: this.inputData.substr(8, 64) === '0000000000000000000000004ecd812b010d9db16b0fb7143a79786b65b89b09',
+          amount: Web3.utils.fromWei(Web3.utils.toBN('0x' + this.inputData.substr(72, 64)).toString()),
+          aeternity: new Web3().utils.hexToString((new Web3()).eth.abi.decodeParameter('bytes', this.inputData.substr(136)))
+        }
+      } catch (e) {
+        /* handle error */
+        return e
       }
     },
     ...mapState([
-      'web3'
+      'walletAddress'
     ])
   }
 }
