4856ae82686cbee0f498a85d5b7f1aaa55b5e090
Sadi Qevani <inaveq@gmail.com>
2018-11-14 17:15:08 +0100

    Adding annotations

--- src/mixins/scan.js
+++ src/mixins/scan.js
@@ -1,31 +1,54 @@
 import { QrcodeReader } from 'vue-qrcode-reader'
 
 export default {
+  /**
+   * Importing QRCode component
+   */
   components: { QrcodeReader },
   data: function () {
     return {
-      scanner: true,
-      scannerError: false,
-      paused: false
+      scanner: true, // Is scanner open or not.
+      scannerError: false, // Variable to hold errors in the scanner
+      paused: false // Is camera enabled or not.
     }
   },
   methods: {
+    /**
+     * After onDecode the function gets executed
+     * and returns the payload from the QR Code
+     * @param content
+     */
     onDecode (content) {
       this.$store.commit('setWalletAddress', content)
       this.paused = true
       this.scanner = false
     },
+
+    /**
+     * Function closes the scanner and pushes
+     * the user to a different page if route is provided
+     */
     closeScanner (goTo) {
       this.$store.commit('setWalletAddress', null)
       this.paused = true
       this.scanner = false
       this.$router.push({ name: goTo })
     },
+
+    /**
+     * Opens the scan feature and resets the wallet
+     * address
+     */
     toggleRescan () {
       this.$store.commit('setWalletAddress', null)
       this.paused = false
       this.scanner = true
     },
+
+    /**
+     * On execution of the QR Code library
+     * check if something went wrong
+     */
     async onInit (promise) {
       try {
         await promise
